a2871e4c8f82a9bea547c2cca527b4dfe2098d6b
ag10 <a.glodek21@gmail.com>
2025-07-07 17:37:07 +0800

    OPCODEs disassembly

--- pic12f1840-disassembler.c
+++ pic12f1840-disassembler.c
@@ -19,10 +19,33 @@ struct Instruction
 
 const struct Operation OPERATION_TABLE[] = 
 {
-    {0b100u , "CALL", 3},
-    {0b101u , "GOTO", 3},
-    {0b0100u, "BCF" , 4},
-};
+    {0b000111u,         "ADDWF",   6},
+    {0b111101u,         "ADDWFC",  6},
+    {0b000101u,         "ANDWF",   6},
+    {0b110111u,         "ASRF",    6},
+    {0b110101u,         "LSLF",    6},
+    {0b110110u,         "LSRF",    6},
+    {0b0000011u,        "CLRF",    7},
+    {0b00000100000u,    "CLRW",   11},
+    {0b001001u,         "COMF",    6},
+    {0b000011u,         "DECF",    6},
+    {0b001010u,         "INCF",    6},
+    {0b000100u,         "IORWF",   6},
+    {0b001000u,         "MOVF",    6},
+    {0b0000001u,        "MOVWF",   7},
+    {0b001101u,         "RLF",     6},
+    {0b001100u,         "RRF",     6},
+    {0b000010u,         "SUBWF",   6},
+    {0b111011u,         "SUBWFB",  6},
+    {0b001110u,         "SWAPF",   6},
+    {0b000110u,         "XORWF",   6},
+    {0b001011u,         "DECFSZ",  6},
+    {0b001111u,         "INCFSZ",  6},
+    {0b0100u,           "BCF" ,    4},
+    {0b0101u,           "BSF" ,    4},
+    {0b100u ,           "CALL",    3},
+    {0b101u ,           "GOTO",    3},
+}; 
 const int OPERATION_TABLE_LENGTH = sizeof(OPERATION_TABLE) / sizeof(struct Operation);
 
 const struct Operation *getOperation(uint bytes)
@@ -30,7 +53,15 @@ const struct Operation *getOperation(uint bytes)
     for(int i = 0; i < OPERATION_TABLE_LENGTH; ++i)
     {
         const struct Operation *operation = &OPERATION_TABLE[i];
-        if(operation->opcode << (INSTRUCTION_LENGTH - operation->opcodeLength) == bytes & (0xffffu << (INSTRUCTION_LENGTH - operation->opcodeLength))) return operation;
+        printf("instruction: %x\n", bytes);
+        uint truncatedInstruction = bytes & (0xffffu << (INSTRUCTION_LENGTH - operation->opcodeLength));
+        printf("truncated instruction: %x\n", truncatedInstruction);
+        uint offsetOpcode = operation->opcode << (INSTRUCTION_LENGTH - operation->opcodeLength);
+        printf("offset opcode: %x\n", offsetOpcode);
+        if(offsetOpcode == truncatedInstruction)
+        {
+            return operation;
+        }
     }
     return NULL;
 }
@@ -38,9 +69,12 @@ const struct Operation *getOperation(uint bytes)
 //Ignores the 7th and 8th least significant bit, and returns the instruction as an unsigned 14-bit integer
 uint formatBytes(uint bytes)
 {
-    uint high = 0xff00u & bytes;
-    uint low = UINT8_C(0x003fu) & bytes;
-    high = high >> 2;
+    uint low = 0x3f00u & bytes;
+    //printf("low: %x\n", low);
+    uint high = 0x00ffu & bytes;
+    //printf("high: %x\n", high);
+    high = high << 6;
+    low = low >> 8;
     return high | low;
 }
 
@@ -48,9 +82,10 @@ uint formatBytes(uint bytes)
 struct Instruction readInstruction(FILE *stream)
 {
     uint bytes;
-    const int offset = __INT_WIDTH__ / 8 - 2;
-    fread(&bytes + offset, 2, 1, stream);
+    fread(&bytes, 2, 1, stream);
+    //printf("raw: %x\n", bytes);
     bytes = formatBytes(bytes);
+    //printf("formatted: %x\n", bytes);
     struct Instruction instruction =
     {
         getOperation(bytes)
@@ -58,11 +93,20 @@ struct Instruction readInstruction(FILE *stream)
     return instruction;
 }
 
-//Write an instruction to stdout
-void writeInstruction(struct Instruction *instruction)
+//Write an instruction to stream
+void writeInstruction(struct Instruction *instruction, FILE *stream)
 {
-    if(!instruction) puts("ERROR");
-    else puts(instruction->operation->mnemonic);
+    if(!instruction->operation)
+    {
+        //puts("aawawawa");
+        fputs("ERROR\n", stream);
+        //puts("awa2");
+    }
+    else
+    {
+        fputs(instruction->operation->mnemonic, stream);
+        fputc('\n', stream);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -70,15 +114,22 @@ int main(int argc, char *argv[])
     switch(argc) {
         case 1:
         case 2:
-            fputs("pic12f1850-disassembler - Usage:\npic12f1850-disassembler [input filename] [output filename]", stdout);
-            break;
+        fputs("pic12f1850-disassembler\n\nUsage:\npic12f1850-disassembler (input filename) (output filename)\n", stdout);
+        return 0;
+        break;
     }
-    FILE *file = fopen(argv[1], "r");
-    FILE *file = fopen(argv[2], "w");
-    for(int i = 0; i < MEMORY_SIZE; ++i) 
+    FILE *inFile = fopen(argv[1], "r+");
+    if(!inFile)
     {
-        struct Instruction instruction = readInstruction(file);
-        writeInstruction(&instruction);
+        fputs("Can't open file\n", stderr);
+        return -1;
     }
+    FILE *outFile = fopen(argv[2], "w+");
+    for(int i = 0; i < MEMORY_SIZE/2; ++i) 
+    {
+        struct Instruction instruction = readInstruction(inFile);
+        writeInstruction(&instruction, outFile);
+    }
+    fclose(inFile);
+    fclose(outFile);
 }
-
